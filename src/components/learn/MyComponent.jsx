9df64c0ef00552f7bee6659db915a3af74aa354d
huylyanh <huylyanh@yahoo.com>
2024-10-02 23:12:26 +0700

    #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,24 @@
 //trong React: component la 1 cach de thao tac voi html
 // --> viet MyComponent duoi dang tag
 // Ten component phai viet hoa
+
+//JSX: cong nghe giúp tu dich code html, dich javascrip bien thanh html
+
+import './style.css';
 const MyComponent = () => {
     // trong arrow function phai return va tra ra html
     return (
-        <div>huy & first react</div>
+        // 0) JSX chi co 1 parent --> fragment de fix
+        // 1) Chi tra ra 1 phan tu duy nhat ben trong JSX
+        // 2) Dung fragment: ko render thua thai html div
+        <>
+            <div>huy & first react</div>
+            <div className="child"
+                style={
+                    { borderRadius: "10px" }
+                }
+            >child</div>
+        </>
     );
 }
 
